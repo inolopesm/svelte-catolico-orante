dcb9d308f6ed742033dd5720295a0368958fd057
Matheus Inocêncio Lopes <inolopesm@gmail.com>
2023-02-12 14:00:53 -0300

    chore: install and configure tailwind

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,6 @@
   const message = "Hello World";
 </script>
 
-<main>
-  <h1>Hello World!</h1>
+<main class="flex justify-center items-center h-screen bg-gray-50">
+  <h1 class="text-4xl text-gray-900">Hello World!</h1>
 </main>
